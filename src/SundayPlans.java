8dc090307e38a7d10a2591f03fb86778eebca988
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-02-11 19:13:46 +0530

    weekend SundayPlansAdded Added AnotherPlans

---
+++ src/SundayPlans.java
@@ -0,0 +1,17 @@
+public class SundayPlans {
+    private String sleep;
+    private String washDresses;
+
+    public SundayPlans(String sleep, String washDresses) {
+        this.sleep = sleep;
+        this.washDresses = washDresses;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getWashDresses() {
+        return washDresses;
+    }
+}
