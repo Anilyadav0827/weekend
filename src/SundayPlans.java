253bf16bcd373d01049707068a640da5841407b1
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-02-11 19:05:54 +0530

    weekend SundayPlansAdded

---
+++ src/SundayPlans.java
@@ -0,0 +1,17 @@
+public class SundayPlans {
+    private String party;
+    private String completeWork;
+
+    public SundayPlans(String party, String completeWork) {
+        this.party = party;
+        this.completeWork = completeWork;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public String getCompleteWork() {
+        return completeWork;
+    }
+}
